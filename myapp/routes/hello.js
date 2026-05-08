f44623a777aeb65da2c697509ba006b3bc153487
Keito Kobayashi <xiaolinxiren@gmail.com>
2026-05-08 16:55:03 +0900

    add hello

--- myapp/routes/hello.js
+++ myapp/routes/hello.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/hello', function(req,res,next){
+router.get('/', function(req,res,next){
     res.render('hello', {hoge: 'Express World'});
 })
 
