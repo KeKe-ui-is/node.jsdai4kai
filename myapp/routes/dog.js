4212f05c11cdcbcccd2bd6c1eb4072f3aa993ecd
Keito Kobayashi <xiaolinxiren@gmail.com>
2026-05-31 12:22:38 +0900

    dogAPIの実装

--- myapp/routes/dog.js
+++ myapp/routes/dog.js
@@ -6,23 +6,7 @@ router.get('/', async (req, res) => {
         body) {
         if (!error && response.statusCode == 200) {
             const data = JSON.parse(body);
-            // messageに犬の画像URL
-            const imageUrl = data.message;
-
-            res.send(`
-            <!DOCTYPE html>
-            <html lang="ja">
-            <head>
-                <meta charset="UTF-8">
-                <title>犬の画像</title>
-            </head>
-            <body>
-                <h1>ランダムな犬の画像</h1>
-                <p>${JSON.stringify(data, null, 2)}</p>
-                <img src="${imageUrl}" alt="犬の画像" width="400">
-            </body>
-            </html>
-        `);
+            res.json(data);
         }
     });
 })
