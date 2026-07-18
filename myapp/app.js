2032518ca72b7c931af5eaffc6b9b4f0bcacc1e2
Keito Kobayashi <xiaolinxiren@gmail.com>
2026-07-18 21:05:43 +0900

    from_b add

--- myapp/app.js
+++ myapp/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
+var notesFromBRouter = require('./routes/notes_from_b');
 var catRouter = require('./routes/cat');
 var dogRouter = require('./routes/dog');
 
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
+app.use('/notes_from_b', notesFromBRouter);
 app.use('/cat',catRouter);
 app.use('/dog',dogRouter);
 
